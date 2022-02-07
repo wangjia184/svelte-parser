0d2a3be354b4e26037bc9f1cce660aa54b618efe
Jerry.Wang <jerry.wang@everymatrix.com>
2022-02-07 10:31:55 +0800

    + prev commit

--- src/worker.ts
+++ src/worker.ts
@@ -20,11 +20,29 @@ async function parseSvelte(source : string) {
   const preprocessedResult = await preprocess( source, [
     typescript({ 
       "compilerOptions": {
-        "allowSyntheticDefaultImports": true,
-        "noImplicitAny": true,
-        "module": "esnext",
-        "target": "es5",
-        "allowJs": true
+        "moduleResolution": "node",
+        "target": "es2017",
+        /**
+          Svelte Preprocess cannot figure out whether you have a value or a type, so tell TypeScript
+          to enforce using `import type` instead of `import` for Types.
+        */
+        "importsNotUsedAsValues": "error",
+        /**
+          TypeScript doesn't know about import usages in the template because it only sees the
+          script of a Svelte file. Therefore preserve all value imports. Requires TS 4.5 or higher.
+        */
+        "preserveValueImports": true,
+        "isolatedModules": true,
+        /**
+          To have warnings/errors of the Svelte compiler at the correct position,
+          enable source maps by default.
+        */
+        "sourceMap": true,
+
+        "strict": false,
+        "esModuleInterop": true,
+        "skipLibCheck": true,
+        "forceConsistentCasingInFileNames": true
       }
      }),
     scss(),
